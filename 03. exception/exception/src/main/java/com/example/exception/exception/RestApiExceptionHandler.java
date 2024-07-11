9dcd0c3904fa7b1d34986238d25949017121ac19
limbaba1120 <kwlim1120ss@gmail.com>
2024-07-11 12:25:13 +0900

    feat(exception): add exception handler (global) for User API
    - add GlobalExceptionHandler to handle all not found exception
    - set exception  priority using @Order

--- 03. exception/exception/src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
+++ 03. exception/exception/src/main/java/com/example/exception/exception/RestApiExceptionHandler.java
@@ -1,26 +1,48 @@
 package com.example.exception.exception;
 
+import com.example.exception.model.Api;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.NoSuchElementException;
+
 @Slf4j
 @RestControllerAdvice(basePackages = "com.example.exception.controller")
+@Order(1)
 public class RestApiExceptionHandler {
 
-    @ExceptionHandler(value = {Exception.class})
-    public ResponseEntity exception(Exception e) {
-        log.error("Exception", e);
-
-        return ResponseEntity.status(200).body("ok");
-    }
+//    @ExceptionHandler(value = {Exception.class})
+//    public ResponseEntity exception(Exception e) {
+//        log.error("Exception", e);
+//
+//        return ResponseEntity.status(200).body("ok");
+//
+//    }
 
     // 원하는 예외 잡기
     @ExceptionHandler(value = {IndexOutOfBoundsException.class})
     public ResponseEntity outOfBound(IndexOutOfBoundsException e) {
         log.error("IndexOutOfBoundsException", e);
+
         return ResponseEntity.status(200).build();
 
+    }
+
+    @ExceptionHandler(value = {NoSuchElementException.class})
+    public ResponseEntity<Api> noSuchElement(NoSuchElementException e) {
+        log.error("NoSuchElementException", e);
+
+        Api<Object> response = Api.builder()
+                .resultCode(String.valueOf(HttpStatus.NOT_FOUND.value()))
+                .resultMessage(HttpStatus.NOT_FOUND.getReasonPhrase())
+                .build();
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
     }
 }
