a76d55f71167e62c5a7ee1c411628adc85791d35
limbaba1120 <kwlim1120ss@gmail.com>
2024-07-13 17:28:40 +0900

    feat: add pagination support for post API
    - develop PostService to handle pagination and return Api response with
      pagination details

--- 08. simpleboard/simple-board/src/main/java/com/example/simpleboard/board/model/BoardDto.java
+++ 08. simpleboard/simple-board/src/main/java/com/example/simpleboard/board/model/BoardDto.java
@@ -1,2 +1,31 @@
-package com.example.simpleboard.board.model;public class BoardDto {
+package com.example.simpleboard.board.model;
+
+import com.example.simpleboard.post.db.PostEntity;
+import com.example.simpleboard.post.model.PostDto;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BoardDto {
+
+    private Long id;
+
+    private String boardName;
+
+    private String status;
+
+    private List<PostDto> postList = List.of();
 }
--- 08. simpleboard/simple-board/src/main/java/com/example/simpleboard/common/Pagination.java
+++ 08. simpleboard/simple-board/src/main/java/com/example/simpleboard/common/Pagination.java
@@ -1,2 +1,22 @@
-package com.example.simpleboard.common;public class Pagination {
+package com.example.simpleboard.common;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Pagination {
+
+    private Integer page;
+
+    private Integer size;
+
+    private Integer currentElements;
+
+    private Integer totalPage;
+
+    private Long totalElements;
 }
